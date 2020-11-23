44f374023ea5ef06f9dfc50072d55f9c48852bd6
Tony Alejandro <tonysviews@gmail.com>
2020-11-23 08:50:45 +0800

    add dashboard entity count

--- pages/MainDashboard.js
+++ pages/MainDashboard.js
@@ -36,20 +36,22 @@ export default class MainDashboard extends LitElement {
     this.entities = [];
     this.count = {};
   }
-  
+
   render() {
     return html`
       <page-layout>
         <mv-container>
           <h1>Dashboard</h1>
           <div class="tiles">
-            ${this.entities.map(entity => html`
-            <dashboard-tile
-              entity-code="${entity.code}"
-              title="${entity.label}"
-              item-count="${this.count[entity.code] || 0}"
-            ></dashboard-tile>
-            `)}
+            ${this.entities.map(
+              (entity) => html`
+                <dashboard-tile
+                  entity-code="${entity.code}"
+                  title="${entity.label}"
+                  item-count="${this.count[entity.code] || 0}"
+                ></dashboard-tile>
+              `
+            )}
           </div>
         </mv-container>
       </page-layout>
@@ -57,10 +59,42 @@ export default class MainDashboard extends LitElement {
   }
 
   connectedCallback() {
-    this.entities = extractEntities(config) ||[];
+    this.entities = extractEntities(config) || [];
     // call api to load entity counts
+    this.entities.forEach((entity) => {
+      const endpointInterface = new EndpointInterface(
+        entity.code,
+        "POST",
+        "LIST"
+      );
+      endpointInterface.executeApiCall(
+        this,
+        {
+          noAuth: true,
+          config,
+          firstRow: 0,
+          numberOfRows: 20,
+          fetchFields: this.columns,
+        },
+        this.submitSuccess(entity),
+        this.submitFailed(entity)
+      );
+    });
     super.connectedCallback();
   }
+
+  submitSuccess = (entity) => (event) => {
+    this.count = {
+      ...this.count,
+      [entity.code]: event.detail.result.length || 0,
+    };
+  };
+
+  submitFailed = (entity) => (event) => {
+    console.log("Encountered error while retrieving list details.");
+    console.log("entity: ", entity);
+    console.log("event.detail: ", JSON.stringify(event.detail, null, 2));
+  };
 }
 
 customElements.define("main-dashboard", MainDashboard);
