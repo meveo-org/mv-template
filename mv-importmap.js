e03eee5e8ea8f418d8054af3fb6baa053eaa026b
Tony Alejandro <tonysviews@gmail.com>
2022-03-23 18:26:02 +0800

    import router-slot from mv-dependencies

--- mv-importmap.js
+++ mv-importmap.js
@@ -1,5 +1,6 @@
 window.importmap.imports.utils = "./utils/index.js";
 window.importmap.imports.config = "./config.js";
+window.importmap.imports['router-slot'] = "./node_modules/@meveo-org/mv-dependencies/router-slot/index.js";
 const im = document.createElement('script');
 im.type = 'importmap';
 im.textContent = JSON.stringify(importmap);
